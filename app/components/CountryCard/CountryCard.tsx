10fdc29f68d9754c64f3b48dc002bc794ddf5802
3ARR4 <barrazaaxel2000@gmail.com>
2025-03-12 14:26:16 -0600

    good v1 for country card on dashboard

--- app/components/CountryCard/CountryCard.tsx
+++ app/components/CountryCard/CountryCard.tsx
@@ -1,17 +1,32 @@
 const CountryCard = () => {
   return (
-    <div className="w-100 d-flex flex-column text-white ps-4 pt-2">
-      <div className="d-flex gap-4">
-        <h1>China</h1>
-        <div className="d-flex align-items-center ms-2">
-          <i className="fa-solid fa-people-group fa-2xl me-2"></i>
-          <p className="m-0">1,425,179,569</p>
+    <div className="w-100 h-100  d-flex justify-content-evenly">
+      <div className="container-content h-100  d-flex flex-column text-white pt-4">
+        <div className="d-flex mb-auto">
+          <h1 style={{ paddingRight: "32px" }}>China</h1>
+          <div className="d-flex align-items-center">
+            <i className="fa-solid fa-people-group fa-2xl me-2"></i>
+            <p className="m-0">1,425,179,569</p>
+          </div>
+        </div>
+        <div className="d-flex w-100 align-self-baseline justify-content-between" style={{marginBottom:"32px"}}>
+          <div className="d-flex flex-column">
+            <p className="data-highlight m-0">12,667,428,430 T</p>
+            <p className="m-0">CO2 Emissions</p>
+          </div>
+          <div className="d-flex flex-column">
+            <p className="data-highlight m-0">-1.04 °C</p>
+            <p className="m-0">Temperature Change</p>
+          </div>
+
+          <div className="d-flex flex-column">
+            <p className="data-highlight m-0">3.5 mm</p>
+            <p className="m-0">Sea Level Rise </p>
+          </div>
         </div>
       </div>
-      <div className="d-flex justify-content-evenly">
-        <p>12,667,428,430 T</p>
-        <p>12,667,428,430 T</p>
-        <p>12,667,428,430 T</p>
+      <div className="d-flex h-100 align-items-center">
+        <i className="fas fa-chevron-right text-white cursor-pointer"> </i>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,16 @@ import "./globals.css";
 export default function Home() {
   return (
     <div>
-      (
+   
       <main>
         <Header />
-        <div className="pt-4 d-flex justify-content-center">
+        <div className="spacing-card d-flex justify-content-center">
           <div className="cards-container">
             <CountryCard />
           </div>
         </div>
       </main>
-      );
+  
     </div>
   );
 }
