f5dcdd8e1ce96a2c80df722bd9b5137db7f02cb6
3ARR4 <barrazaaxel2000@gmail.com>
2025-03-13 11:38:24 -0600

    api call added for rise in sea level

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import {
   cO2DataType,
   tempChangeResponseType,
   tempChangeType,
-  mainClimateChangeDataType
+  mainClimateChangeDataType,
 } from "./types/apiResponseType";
 
 import axios from "axios";
@@ -18,20 +18,27 @@ export default function Home() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [co2Response, tempResponse] = await Promise.all([
-          axios.get(
-            `https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Indicator_2_Carbon_Emission_per_unit_of_Output/FeatureServer/0/query?where=Unit%20%3D%20'MILLIONS%20OF%20METRIC%20TONS%20OF%20CO2'%20AND%20Industry%20%3D%20'ELECTRICITY,%20GAS,%20STEAM%20AND%20AIR%20CONDITIONING%20SUPPLY'&outFields=Country,Unit,Source,F2018,F2017,F2016,F2015,F2014,F2013,F2012,F2011,F2010,F2009,F2008,Industry&outSR=4326&f=json&resultRecordCount=100`
-          ),
-          axios.get(
-            `https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Indicator_3_1_Climate_Indicators_Annual_Mean_Global_Surface_Temperature/FeatureServer/0/query?where=1%3D1&outFields=Country,Unit,Source,F2008,F2009,F2010,F2011,F2012,F2013,F2014,F2015,F2016,F2017,F2018,F2019,F2020,F2021,F2022,F2023&outSR=4326&f=json&resultRecordCount=100`
-          ),
-        ]);
+        const [co2Response, tempResponse, seaLevelResponse] = await Promise.all(
+          [
+            axios.get(
+              `https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Indicator_2_Carbon_Emission_per_unit_of_Output/FeatureServer/0/query?where=Unit%20%3D%20'MILLIONS%20OF%20METRIC%20TONS%20OF%20CO2'%20AND%20Industry%20%3D%20'ELECTRICITY,%20GAS,%20STEAM%20AND%20AIR%20CONDITIONING%20SUPPLY'&outFields=Country,Unit,Source,F2018,F2017,F2016,F2015,F2014,F2013,F2012,F2011,F2010,F2009,F2008,Industry&outSR=4326&f=json&resultRecordCount=100`
+            ),
+            axios.get(
+              `https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Indicator_3_1_Climate_Indicators_Annual_Mean_Global_Surface_Temperature/FeatureServer/0/query?where=1%3D1&outFields=Country,Unit,Source,F2008,F2009,F2010,F2011,F2012,F2013,F2014,F2015,F2016,F2017,F2018,F2019,F2020,F2021,F2022,F2023&outSR=4326&f=json&resultRecordCount=100`
+            ),
+            axios.get(
+              `https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/Indicator_3_3_melted_new/FeatureServer/0/query?where=1%3D1&outFields=Country,Indicator,Unit,Source,Measure,Value&outSR=4326&f=json&resultRecordCount=50`
+            ),
+          ]
+        );
         const co2Data = co2Response.data.features.map(
           (item: cO2ResponseType) => item.attributes
         );
         const tempData = tempResponse.data.features.map(
           (item: tempChangeResponseType) => item.attributes
         );
+
+        console.log("seaLevelResponse: ", seaLevelResponse);
 
         // Combinar los datos solo para países que estén en ambas listas
         const combinedData = co2Data
@@ -86,7 +93,7 @@ export default function Home() {
             }
             return null; // Si no hay coincidencia, devolvemos null
           })
-          .filter((item:mainClimateChangeDataType) => item !== null); // Filtramos los null
+          .filter((item: mainClimateChangeDataType) => item !== null); // Filtramos los null
 
         setResponse(combinedData);
       } catch (error) {
@@ -102,7 +109,7 @@ export default function Home() {
       <main>
         <div className="spacing-card d-flex flex-column align-items-center gap-4">
           {response.length > 0 &&
-            response.map((item:mainClimateChangeDataType) => (
+            response.map((item: mainClimateChangeDataType) => (
               <CountryCard key={item.Country} countryItem={item} />
             ))}
         </div>
